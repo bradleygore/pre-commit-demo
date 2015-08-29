560f975d9758081aed657e5bfc1d480b6452dd14
Bradley Gore <bgore@unicon.net>
2015-08-29 13:55:51 -0500

    Some schweeet code

--- js/example.js
+++ js/example.js
@@ -3,23 +3,23 @@
     'use strict'; //Supposed to use strict mode - (un)comment and see what happens when trying to commit
 
     //Not supposed to use eval - (un)comment and see what happens when trying to commit
-    eval('true == true');
+    //eval('true == true');
 
     //Supposed to use functions only after declaration - swap the order of the following lines and see what happens
-    init();
 
-    function init() {
-        //also, this is an empty block
-    };
+    //function init() {
+    //    //also, this is an empty block
+    //};
+    //init();
 
     //unused variable
-    var someNum = 5;
+    //var someNum = 5;
 
     //Supposed to use triple equals (===)
-    if ('5' == 5) {
+    //if ('5' == 5) {
         //also this is an empty block
-    }
+    //}
 
     //Supposed to use single quotes
-    var newBlah = "I am groot!";
+    //var newBlah = "I am groot!";
 }());
