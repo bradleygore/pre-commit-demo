5be56493d493d3801ab2b0b5f404a494601eb789
Bradley Gore <bgore@unicon.net>
2015-10-06 23:41:13 -0500

    I haz teh broked codez

--- gulp-tasks/test.js
+++ gulp-tasks/test.js
@@ -1,8 +1,5 @@
 var Server = require('karma').Server;
 
-/**
- * Run all tests once and exit
- */
 module.exports = function (done) {
     new Server({
         configFile: __dirname + '/../karma.conf.js',
